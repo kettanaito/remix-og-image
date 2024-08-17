d1d880f34636db9dca169d8c54a24a439c419b72
Artem Zakharchenko <kettanaito@gmail.com>
2024-08-17 15:23:34 +0200

    fix: support vite build

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,13 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { type Plugin, type ResolvedConfig, normalizePath } from 'vite'
+import {
+  type Plugin,
+  type ResolvedConfig,
+  type ViteDevServer,
+  createServer,
+  resolveConfig,
+  normalizePath,
+} from 'vite'
 import { parse as esModuleLexer } from 'es-module-lexer'
 import type { ResolvedRemixConfig } from '@remix-run/dev'
 import type { ConfigRoute } from '@remix-run/dev/dist/config/routes.js'
@@ -10,7 +17,7 @@ import {
   findReferencedIdentifiers,
 } from 'babel-dead-code-elimination'
 import { compile } from 'path-to-regexp'
-import { launch } from 'puppeteer'
+import { Browser, launch } from 'puppeteer'
 import { parse, traverse, generate, t } from './babel.js'
 import {
   OPEN_GRAPH_USER_AGENT_HEADER,
@@ -40,18 +47,22 @@ interface Options {
 interface RemixPluginContext {
   remixConfig: ResolvedRemixConfig
 }
-;``
+
+interface GeneratedOpenGraphImage {
+  name: string
+  content: Uint8Array
+}
+
+const PLUGIN_NAME = 'remix-og-image-plugin'
 const EXPORT_NAME = 'openGraphImage'
 
 export function openGraphImagePlugin(options: Options): Plugin {
+  const format = options.format || 'jpeg'
+
   const viteConfigPromise = new DeferredPromise<ResolvedConfig>()
   const remixContextPromise = new DeferredPromise<RemixPluginContext>()
   const serverUrlPromise = new DeferredPromise<URL>()
   const routesWithImages = new Array<ConfigRoute>()
-
-  const format = options.format || 'jpeg'
-
-  const browserPromise = launch({ headless: true })
 
   async function getOutputDirectory() {
     const viteConfig = await viteConfigPromise
@@ -62,12 +73,15 @@ export function openGraphImagePlugin(options: Options): Plugin {
     )
   }
 
-  async function generateOpenGraphImages(route: ConfigRoute) {
+  async function generateOpenGraphImages(
+    route: ConfigRoute,
+    browser: Browser,
+    serverUrl: URL
+  ): Promise<Array<GeneratedOpenGraphImage>> {
     if (!route.path) {
-      return
+      return []
     }
 
-    const serverUrl = await serverUrlPromise
     const createRoutePath = compile(route.path)
     const resourceUrl = new URL(route.path, serverUrl)
     // Set the "_data" search parameter so the route can be queried
@@ -86,7 +100,7 @@ export function openGraphImagePlugin(options: Options): Plugin {
      * - OG route returning invalid data;
      */
 
-    const browser = await browserPromise
+    const files: Array<GeneratedOpenGraphImage> = []
 
     for (const data of allData) {
       const page = await browser.newPage()
@@ -106,12 +120,10 @@ export function openGraphImagePlugin(options: Options): Plugin {
 
         const ogImageBoundingBox = await page
           .$(options.elementSelector)
-          .then((element) => {
-            return element?.boundingBox()
-          })
+          .then((element) => element?.boundingBox())
 
         if (!ogImageBoundingBox) {
-          return
+          return []
         }
 
         await page.evaluate((selector) => {
@@ -121,44 +133,57 @@ export function openGraphImagePlugin(options: Options): Plugin {
           }
         }, options.elementSelector)
 
-        const screenshot = await page.screenshot({
+        const imageBuffer = await page.screenshot({
           type: format,
           quality: 100,
           encoding: 'binary',
           clip: ogImageBoundingBox,
         })
 
-        const outputDirectory = await getOutputDirectory()
-
-        if (!fs.existsSync(outputDirectory)) {
-          await fs.promises.mkdir(outputDirectory, { recursive: true })
-        }
-
-        await fs.promises.writeFile(
-          path.resolve(outputDirectory, `${data.name}.jpg`),
-          screenshot
-        )
+        files.push({
+          name: `${data.name}.${format}`,
+          content: imageBuffer,
+        })
       } finally {
+        /**
+         * @fixme The entire page screenshot logic can be encapsulated in a Promise
+         * and pushed into some array, then awaited throughout all the routes.
+         */
         await page.close({ runBeforeUnload: false })
       }
     }
+
+    return files
+  }
+
+  async function writeImageToDisk(image: GeneratedOpenGraphImage) {
+    const filePath = path.resolve(await getOutputDirectory(), image.name)
+    const directoryName = path.dirname(filePath)
+    if (!fs.existsSync(directoryName)) {
+      await fs.promises.mkdir(directoryName, { recursive: true })
+    }
+    await fs.promises.writeFile(filePath, image.content)
   }
 
   return {
-    name: 'vite-remix-og-image-plugin',
-    config(config) {
-      remixContextPromise.resolve(Reflect.get(config, '__remixPluginContext'))
-    },
+    name: PLUGIN_NAME,
+
     configResolved(config) {
       viteConfigPromise.resolve(config)
+      /**
+       * @todo Would be nice for Remix to expose this internal key.
+       */
+      remixContextPromise.resolve(Reflect.get(config, '__remixPluginContext'))
     },
-    configureServer(server) {
+
+    async configureServer(server) {
       const { httpServer } = server
 
       if (!httpServer) {
         return
       }
 
+      // In serve mode, listen to the existing preview server.
       httpServer.once('listening', async () => {
         const address = httpServer.address()
 
@@ -174,7 +199,14 @@ export function openGraphImagePlugin(options: Options): Plugin {
         serverUrlPromise.resolve(new URL(url))
       })
     },
+
     async transform(code, id, options = {}) {
+      const viteConfig = await viteConfigPromise
+
+      if (viteConfig.preview.headers?.['x-foo']) {
+        return
+      }
+
       const remixContext = await remixContextPromise
 
       if (!remixContext) {
@@ -203,8 +235,6 @@ export function openGraphImagePlugin(options: Options): Plugin {
       if (!ogImageExport) {
         return
       }
-
-      this.debug(`found og image route: ${route.id}`)
 
       // OG image generation must only happen server-side.
       if (!options.ssr) {
@@ -235,16 +265,128 @@ export function openGraphImagePlugin(options: Options): Plugin {
         return generate(ast, { sourceMaps: true, sourceFileName: id }, code)
       }
 
-      generateOpenGraphImages(route).then(() => {
-        this.info(`generated og image for route: ${route.id}`)
-      })
+      // In serve mode, generate images alongside the changes to routes.
+      if (viteConfig.command === 'serve' && !viteConfig.isProduction) {
+        const serverUrl = await serverUrlPromise
 
-      routesWithImages.push(route)
+        // Don't await the generation promise, let it run in the background.
+        generateOpenGraphImages(
+          route,
+          await getBrowserInstance(),
+          serverUrl
+        ).then((images) => images.map(writeImageToDisk))
+      } else {
+        // In build mode, collect all the OG image routes to be
+        // visited on build end, when the application build is done.
+        routesWithImages.push(route)
+      }
     },
-    async buildEnd() {
-      // Generate the images on build end, when all the assets are ready.
-      // await Promise.all(routesWithImages.map(generateOpenGraphImages))
-      await browserPromise.then((browser) => browser.close())
+
+    /**
+     * @note Use `writeBundle` and not `closeBundle` so the image generation
+     * time is counted toward the total build time.
+     */
+    writeBundle: {
+      order: 'post',
+      async handler() {
+        const viteConfig = await viteConfigPromise
+
+        if (
+          viteConfig.command === 'build' &&
+          /**
+           * @fixme This is a hacky way of knowing the build end.
+           * The problem is that `closeBundle` will trigger MULTIPLE times,
+           * as there are multiple bundles Remix builds (client, server, etc).
+           * This plugin has to run after the LASTMOST bundle.
+           */
+          viteConfig.build.rollupOptions.input ===
+            'virtual:remix/server-build' &&
+          routesWithImages.length > 0
+        ) {
+          console.log(
+            'Generating OG images for %d route(s)...',
+            routesWithImages.length
+          )
+
+          const [browser, server] = await Promise.all([
+            getBrowserInstance(),
+            runVitePreviewServer(viteConfig),
+          ])
+          const serverUrl = new URL(server.resolvedUrls?.local?.[0]!)
+
+          const pendingScreenshots = routesWithImages.map((route) => {
+            return generateOpenGraphImages(route, browser, serverUrl).then(
+              (images) => Promise.all(images.map(writeImageToDisk))
+            )
+          })
+
+          await Promise.allSettled(pendingScreenshots)
+
+          /**
+           * @todo Print out any errored image generation promises.
+           */
+
+          await Promise.all([server.close(), browser.close()])
+        }
+      },
     },
   }
+}
+
+let browser: Browser | undefined
+
+async function getBrowserInstance(): Promise<Browser> {
+  if (browser) {
+    return browser
+  }
+
+  browser = await launch({ headless: true })
+  return browser
+}
+
+async function runVitePreviewServer(
+  viteConfig: ResolvedConfig
+): Promise<ViteDevServer> {
+  /**
+   * @note Force `NODE_ENV` to be "development" for the preview server.
+   * Vite sets certain internal flags based on the environment, and there
+   * is no way to override that. The build is triggered with "production",
+   * but the preview server MUST be "development". This also makes sure
+   * that all the used plugins are instantiated correctly.
+   */
+  process.env.NODE_ENV = 'development'
+
+  // Use the `resolveConfig` function explicitly because it
+  // allows passing options like `command` and `mode` to Vite.
+  const previewViteConfig = await resolveConfig(
+    {
+      root: viteConfig.root,
+      configFile: viteConfig.configFile,
+      /**
+       * @fixme Despite the log levels, this server stills prints
+       * "Using vars defined in XYZ" when run. Is there a way to turn that off?
+       */
+      logLevel: 'error',
+      preview: {
+        /**
+         * @fixme This is a weird way of making sure this plugin
+         * doesn't run in the preview mode spawned during the build.
+         * Would be nice if Vite respected custom `plugins` but it doesn't.
+         * It will ALAWAYS take `plugins` from your app's config.
+         */
+        headers: { 'x-foo': '1' },
+      },
+    },
+    'serve',
+    /**
+     * @note Using `production` mode is important.
+     * It will skip all the built-in development-oriented plugins in Vite.
+     */
+    'production',
+    'development',
+    false
+  )
+
+  const server = await createServer(previewViteConfig.inlineConfig)
+  return server.listen()
 }
